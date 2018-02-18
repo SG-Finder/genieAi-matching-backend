5e173a73047b7c70bcd1258d908b95ae7a3fa4d6
HanTaeWoong <gksxodnd007@gmail.com>
2018-02-18 19:23:28 +0900

    redis host ip 변경

--- genieAiMatching.js
+++ genieAiMatching.js
@@ -131,7 +131,7 @@ matchingSpace.on('connection', function (socket) {
 
     });
 
-    socket.on('ready', function (data) {
+    socket.on('ready', function () {
         //TODO delete event it is just testing event
         if (player[socket.id] == null) {
             socket.emit('retryReady', { error: "connection DB is fail"});
@@ -151,63 +151,64 @@ matchingSpace.on('connection', function (socket) {
     socket.on('gameStart', function (data) {
         //TODO modulation && 동시에 접속했을 때의 이슈 && 매칭이 실패했을 때의 이슈 && 사용자의 수락 이벤트 핸들러
         //TODO 매칭 결과 데이터에 중복된 플레이어가 있을 경우의 이슈 처리(나 자신과의 싸움)
-        let MATCHING_QUE;
-        player[socket.id].matchingActivate = true;
-        player[socket.id].wantsNumOfplayer = data.numOfPlayer;
-        switch (data.numOfPlayer) {
-            case 2:
-                MATCHING_QUE = waitMatchingKey.NumOfPlayerTwo;
-                break;
-            case 3:
-                MATCHING_QUE = waitMatchingKey.NumOfPlayerThree;
-                break;
-            case 4:
-                MATCHING_QUE = waitMatchingKey.NumOfPlayerFour;
-                break;
-            case 5:
-                MATCHING_QUE = waitMatchingKey.NumofPlayerFive;
-                break;
-            default:
-                socket.emit('matchingError', {
-                    code: 400,
-                    message: "Invalid parameter or limit avaliable player"
-                });
-                return;
-        }
-        player[socket.id].matchingQue = MATCHING_QUE;
+        if (player[socket.id].matchingActivate !== true) {
+            let MATCHING_QUE;
+            player[socket.id].matchingActivate = true;
+            player[socket.id].wantsNumOfplayer = data.numOfPlayer;
+            switch (data.numOfPlayer) {
+                case 2:
+                    MATCHING_QUE = waitMatchingKey.NumOfPlayerTwo;
+                    break;
+                case 3:
+                    MATCHING_QUE = waitMatchingKey.NumOfPlayerThree;
+                    break;
+                case 4:
+                    MATCHING_QUE = waitMatchingKey.NumOfPlayerFour;
+                    break;
+                case 5:
+                    MATCHING_QUE = waitMatchingKey.NumofPlayerFive;
+                    break;
+                default:
+                    socket.emit('matchingError', {
+                        code: 400,
+                        message: "Invalid parameter or limit avaliable player"
+                    });
+                    return;
+            }
+            player[socket.id].matchingQue = MATCHING_QUE;
 
-        redisClient.smembers(MATCHING_QUE, function (err, list) {
-            try {
-                if (list.length < data.numOfPlayer - 1) {
-                    redisClient.sadd(MATCHING_QUE, JSON.stringify(player[socket.id]));
-                    socket.emit('waitMatching', {
-                        success: true
-                    })
-                }
-                else if (list.length === (data.numOfPlayer - 1)) {
-                    let matchingPlayer = [];
-                    matchingPlayer[matchingPlayer.length] = player[socket.id];
-                    for (let i = 0; i < (data.numOfPlayer - 1); i++) {
-                        matchingPlayer[matchingPlayer.length] = JSON.parse(list.pop());
-                        redisClient.srem(MATCHING_QUE, JSON.stringify(matchingPlayer[i]));
+            redisClient.smembers(MATCHING_QUE, function (err, list) {
+                try {
+                    if (list.length < data.numOfPlayer - 1) {
+                        redisClient.sadd(MATCHING_QUE, JSON.stringify(player[socket.id]));
+                        socket.emit('waitMatching', {
+                            success: true
+                        })
                     }
+                    else if (list.length === (data.numOfPlayer - 1)) {
+                        let matchingPlayer = [];
+                        matchingPlayer[matchingPlayer.length] = player[socket.id];
+                        for (let i = 0; i < (data.numOfPlayer - 1); i++) {
+                            matchingPlayer[matchingPlayer.length] = JSON.parse(list.pop());
+                            redisClient.srem(MATCHING_QUE, JSON.stringify(matchingPlayer[i]));
+                        }
 
-                    let saveRedisMatchingData = {
-                        playersInfo: matchingPlayer,
-                        roomId: player[socket.id].roomKey
-                        //roomId 생성 (socket.id값으로 인코딩 하기)
-                    };
-                    statusMessageMachine = subscribeMessageType.MATCHING_SUCCESS_MESSAGE;
-                    redisClient.publish(gameLobbyA, JSON.stringify(saveRedisMatchingData));
+                        let saveRedisMatchingData = {
+                            playersInfo: matchingPlayer,
+                            roomId: player[socket.id].roomKey
+                        };
+                        statusMessageMachine = subscribeMessageType.MATCHING_SUCCESS_MESSAGE;
+                        redisClient.publish(gameLobbyA, JSON.stringify(saveRedisMatchingData));
+                    }
                 }
-            }
-            catch(err) {
-                console.log(err);
-                socket.emit('waitMatching', {
-                    success: false
-                });
-            }
-        });
+                catch(err) {
+                    console.log(err);
+                    socket.emit('waitMatching', {
+                        success: false
+                    });
+                }
+            });
+        }
     });
 
     socket.on('cancelMatching', function (data) {
